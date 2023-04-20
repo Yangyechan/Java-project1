2d816696b418717a896018ddae693c7c420a169f
yechanYang <yac392045@mokpo.ac.kr>
2023-04-20 15:15:06 +0900

    feat: 초기화 실습

---
+++ src/main/java/week1/VariableEx.java
@@ -0,0 +1,13 @@
+package week1;
+
+import com.example.project1.PrintHello;
+
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal = 0;
+
+        PrintHello printHello = new PrintHello();
+        System.out.println(iVal);
+        printHello.print();
+    }
+}
