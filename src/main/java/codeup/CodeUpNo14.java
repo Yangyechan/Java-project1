1619a0f2f4b32578eaef0fa902797bf9595728c5
yechanYang <yac392045@mokpo.ac.kr>
2023-04-20 15:34:21 +0900

    feat: code up 1014번

---
+++ src/main/java/codeup/CodeUpNo14.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class CodeUpNo14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.printf("%s %s", b, a);
+    }
+}
