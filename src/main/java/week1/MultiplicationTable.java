78ed87ec41f3326028457a23cca4a157fb0e5aa8
yechanYang <yac392045@mokpo.ac.kr>
2023-04-20 11:28:40 +0900

    feat: printf 추가

---
+++ src/main/java/week1/MultiplicationTable.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // 2 * 1 = 2
+        // 2 * 2 = 4
+        System.out.println("2" + "*" + "1" + "=" + "2");
+        System.out.printf("%d * %d = %d", 2, 2, 4);
+    }
+}
