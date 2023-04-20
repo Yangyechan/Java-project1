8e0e4bf808b5478d1e4aa912efb9189568184752
yechanYang <yac392045@mokpo.ac.kr>
2023-04-20 09:42:39 +0900

    feat: Scanner str 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
@@ -5,4 +6,13 @@ module com.example.demo {
                             
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+=======
+module com.example.project1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.project1 to javafx.fxml;
+    exports com.example.project1;
+>>>>>>> 30e52a8 (first commit)
 }
