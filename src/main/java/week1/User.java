b5fc8f39bcdff246be6d734b0b03c550388ce707
yechanYang <yac392045@mokpo.ac.kr>
2023-04-21 15:15:57 +0900

    feat: User Object 실습

---
+++ src/main/java/week1/User.java
@@ -0,0 +1,7 @@
+package week1;
+
+public class User {
+    String name;
+    String phoneNumber;
+    int age;
+}
---
+++ src/main/java/week1/UserTest.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "양예찬";
+        user.phoneNumber = "010-****-9702";
+        user.age = 26;
+        System.out.println(user.name);
+        System.out.println(user.phoneNumber);
+        System.out.println(user.age);
+    }
+}
