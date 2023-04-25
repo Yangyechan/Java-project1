97462a7d1b7b36a8a75cdbc246e4060c1429746a
yechanYang <yac392045@mokpo.ac.kr>
2023-04-25 13:35:04 +0900

    feat: Code Up 1018,1019

--- src/main/java/codeup/CodeUpNo18.java
+++ src/main/java/codeup/CodeUpNo18.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CodeUpNo18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String time[] = sc.next().split(":");
+        String time[] = sc.next().split(":");//split
         System.out.printf(time[0] +":" +time[1]);
     }
 }
--- src/main/java/codeup/CodeUpNo19.java
+++ src/main/java/codeup/CodeUpNo19.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class CodeUpNo19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String date [] = sc.next().split("\\.");
+        String date [] = sc.next().split("\\.");//split
         int year = Integer.parseInt(date[0]);
         int month = Integer.parseInt(date[1]);
         int day = Integer.parseInt(date[2]);
