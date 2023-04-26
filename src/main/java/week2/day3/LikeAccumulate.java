5d6037a99b87fec6c4841548e718292d780a6e0f
yechanYang <yac392045@mokpo.ac.kr>
2023-04-26 15:35:48 +0900

    feat: 좋아요, 좋아요취소 실습

---
+++ src/main/java/week2/day3/LikeAccumulate.java
@@ -0,0 +1,13 @@
+package week2.day3;
+
+public class LikeAccumulate {
+    public static void main(String[] args) {
+        int likeCount = 0;
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+        likeCount = likeCount + 1;
+        System.out.println(likeCount);
+        likeCount = likeCount - 1;
+        System.out.println(likeCount);
+    }
+}
