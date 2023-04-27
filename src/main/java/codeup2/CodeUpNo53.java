3dc176eb12e887d268afb349f5db58bd7e8c577f
yechanYang <yac392045@mokpo.ac.kr>
2023-04-27 13:24:26 +0900

    feat: codeup 1053~1058

---
+++ src/main/java/codeup2/CodeUpNo53.java
@@ -0,0 +1,15 @@
+package codeup2;
+
+import java.util.Scanner;
+
+public class CodeUpNo53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 0) {
+            System.out.println(1);
+        } else if (a == 1) {
+            System.out.println(0);
+        }
+    }
+}
