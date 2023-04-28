4503595bdd9de80706470957ed215e88c9fc96c3
yechanYang <yac392045@mokpo.ac.kr>
2023-04-28 14:23:53 +0900

    feat: codeup 1119~1121

---
+++ src/main/java/codeup5/CodeUpNo120.java
@@ -0,0 +1,13 @@
+package codeup5;
+
+import java.util.Scanner;
+
+public class CodeUpNo120 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.printf("%.02f",(a + b +c)/3.0);
+    }
+}
